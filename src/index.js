14709637b76a5c14b9dbbd96237374734d38fbfa
1stVyacheslav <komaroff4@gmail.com>
2019-07-18 12:39:17 +0500

    UI for Redux (les.109, sec.10)

--- src/index.js
+++ src/index.js
@@ -4,7 +4,10 @@ const reducer = (state = 0, action) => {
 
 	switch (action.type) {
 		case 'INC':
-			return ++state;
+			return state + 1;
+
+		case 'DEC':
+			return state - 1;
 			
 		default:
 			return state
@@ -13,13 +16,33 @@ const reducer = (state = 0, action) => {
 }
 
 const store = createStore(reducer); // по сути обертка вокруг функции reducer
-store.subscribe( () => {
-	// вывести текущее состояние state
-	console.log( store.getState() )
-} )
+// store.subscribe( () => {
+// 	// вывести текущее состояние state
+// 	console.log( store.getState() )
+// } )
 
 
-// передаем Action
-store.dispatch( {type: 'INC'} )
-store.dispatch( {type: 'INC'} )
-store.dispatch( {type: 'INC'} )
+// // передаем Action
+// store.dispatch( {type: 'INC'} )
+// store.dispatch( {type: 'INC'} )
+// store.dispatch( {type: 'INC'} )
+
+document
+	.getElementById('inc')
+	.addEventListener('click', () => {
+		store.dispatch( {type: 'INC'} )
+	})
+
+document
+	.getElementById('dec')
+	.addEventListener('click', () => {
+		store.dispatch( {type: 'DEC'} )
+	})
+
+function updateCounter() {
+	document
+		.getElementById('counter')
+		.textContent = store.getState()
+}
+
+store.subscribe( () => updateCounter() )
