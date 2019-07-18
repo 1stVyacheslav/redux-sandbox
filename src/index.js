da78e36263e2cfea35fd0e34deea30121ccccd7e
1stVyacheslav <komaroff4@gmail.com>
2019-07-18 16:06:33 +0500

    bindActionCreators() (les.113, sec.10)

--- src/index.js
+++ src/index.js
@@ -1,21 +1,24 @@
-import { createStore } from 'redux';
+import { createStore, bindActionCreators } from 'redux';
 
 import reducer from './reducer';
-import { inc, dec, rnd } from './actions';
+import * as actions from './actions'; // импортировать все в объект actions
 
 const store = createStore(reducer);
+const { dispatch } = store;
+
+// полученные функции эквивалентны store.dispatch( inc() ) и т.п.
+// т.е. bindActionCreators() связывает action creator c функцией dispatch()
+const { inc, dec, rnd } = bindActionCreators(actions, dispatch);
 
 document
 	.getElementById('inc')
 	.addEventListener('click', () => {
-		store.dispatch( inc() )
+		inc() // заменили store.dispatch( inc() )
 	})
 
 document
 	.getElementById('dec')
-	.addEventListener('click', () => {
-		store.dispatch( dec() )
-	})
+	.addEventListener( 'click', () => dec() )
 
 document
 	.getElementById('rnd')
@@ -23,7 +26,7 @@ document
 
 		const value = Math.floor(Math.random() * 10 + 1);
 		
-		store.dispatch( rnd(value) )
+		rnd(value)
 	})
 
 
